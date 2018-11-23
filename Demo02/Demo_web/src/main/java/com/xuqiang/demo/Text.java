edbbbddd752378e1afb3a8049ca6f2f822646cc1
xuqiang <wine_7@yeah.net>
2018-11-23 10:25:23 +0800

    demo02项目

---
+++ Demo02/Demo_web/src/main/java/com/xuqiang/demo/Text.java
@@ -0,0 +1,15 @@
+package com.xuqiang.demo;
+
+
+
+public class Text {
+public static void main(String[] args) {
+	System.out.println("hello world");
+	System.out.println("nihaoa");
+	
+	String string="sss";
+	
+	System.out.println(string+"haha");
+	
+}
+}
